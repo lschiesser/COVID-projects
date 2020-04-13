bd73c3448d657c15fc23f6d0cc3ea3e32a1a3f61
lschiesser <41335130+lschiesser@users.noreply.github.com>
2020-04-13 19:22:00 +0200

    new data

--- record_deaths.py
+++ record_deaths.py
@@ -14,17 +14,18 @@ rows = table.find_all('tr')
 data = []
 for row in rows[1:]:
     cols = row.find_all('td')
-    cols = [ele.text.strip() for ele in cols[:-1]]
+    cols = [ele.text.strip() for ele in cols]
     data.append([ele for ele in cols if ele])
 data = [x for x in data if x != []]
 for row in data:
     row[0] = row[0].replace('\xad','')
+    row[0] = row[0].replace('\n','')
+    row[-1] = row[-1].replace('.', '')
 data = [[x[0],x[-1]] for x in data]
 s = soup.find('div',{'id':'content'})
-retrieval_time = s.find_all('p',class_ = 'null')
-retrieval_time = retrieval_time[0].text
+retrieval_time = s.find('p').text
 retrieval_time = dparser.parse(retrieval_time,fuzzy=True)
-retrieval_time = retrieval_time.strftime('%d.%m.%Y %H:%M')
+retrieval_time = retrieval_time.strftime('%m.%d.%Y %H:%M')
 state_data = pd.DataFrame(data,columns=['Bundesland','Todesfälle'])
 state_data['Todesfälle'] = pd.to_numeric(state_data['Todesfälle'])
 state_to_abb = {'Baden-Württemberg':'BW','Bayern':'BY','Berlin':'BE', 'Brandenburg':'BB','Bremen':'HB','Hamburg':'HH','Hessen':'HE','Mecklenburg-Vorpommern':'MV',
